5e2ffa3f40e303fae827055886d9ac0dd93ad296
Francisco J. Medina <fjmedina21@outlook.com>
2024-11-02 13:22:17 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {registerLicense} from '@syncfusion/ej2-base'
 import { AppModule } from './app/app.module';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1JpRHxbf1x0ZFNMYVRbQXNPIiBoS35RckRjW3pfeHVdQ2VVV0Zz'); // Add your Syncfusion license key here
+registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX9SdkdiWn5XdH1RRWFU'); // Add your Syncfusion license key here
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
